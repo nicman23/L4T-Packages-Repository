3eb5d81a301cc8dbbe14d2a54a9eb9d002e39d33
Azkali <a.ffcc7@gmail.com>
2020-06-27 19:40:03 +0200

    fix: 🐛 Updated stage2 for leap and fedora, switch-configs RPM

--- build-stage2-fedora.sh
+++ build-stage2-fedora.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/bash
 
+echo "Building Nvidia drivers"
+rpmdev-setuptree
+git clone https://github.com/Azkali/L4T-Packages-Repository /L4T-Packages-Repository
+cd /L4T-Packages-Repository/rpmbuilds/nvidia-drivers-package
+dnf builddep nvidia-drivers-package.spec
+rpmbuild -ba nvidia-drivers-package.spec
+cp /root/rpmbuild/RPMS/aarch64/*.rpm /L4T-Packages-Repository/
+echo "Done!"
+
+echo "Building Switch configs"
+cd /L4T-Packages-Repository/rpmbuilds/switch-configs
+cp -r ./* /root/rpmbuild/SOURCES/
+rpmbuild -ba switch-configs.spec
+cp /root/rpmbuild/RPMS/noarch/*.rpm /L4T-Packages-Repository/
+echo "Done!"
+
+echo "Installing XFCE, Nvidia drivers and switch config..."
+dnf -y install /L4T-Packages-Repository/*.rpm
 dnf -y update && dnf -y groupinstall 'Basic Desktop' 'Xfce Desktop'
 dnf -y remove xorg-x11-server-common iscsi-initiator-utils-iscsiuio iscsi-initiator-utils clevis-luks atmel-firmware kernel*
 dnf -y clean all
+
+rm -r /L4T-Packages-Repository/
 
 # TODO: Make kernel rpm
 echo '\nexclude=linux-firmware kernel* xorg-x11-server-* xorg-x11-drv-ati xorg-x11-drv-armsoc xorg-x11-drv-nouveau xorg-x11-drv-ati xorg-x11-drv-qxl xorg-x11-drv-fbdev' >> /etc/dnf/dnf.conf
--- build-stage2-leap.sh
+++ build-stage2-leap.sh
@@ -1,14 +1,35 @@
 #!/usr/bin/bash
-zypper -y dup && zypper -t pattern xfce && zypper -y clean all 
 
+echo "Building Nvidia drivers"
+git clone https://github.com/Azkali/L4T-Packages-Repository /L4T-Packages-Repository
+cd /L4T-Packages-Repository/rpmbuilds/nvidia-drivers-package
+dnf builddep nvidia-drivers-package.spec
+rpmbuild -ba nvidia-drivers-package.spec
+cp /usr/src/packages/RPMS/aarch64/*.rpm /L4T-Packages-Repository/
+echo "Done!"
+
+echo "Building Switch configs"
+cd /L4T-Packages-Repository/rpmbuilds/switch-configs
+cp -r ./* /usr/src/packages/SOURCES/
+rpmbuild -ba switch-configs.spec
+cp /usr/src/packages/RPMS/noarch/*.rpm /L4T-Packages-Repository/
+echo "Done!"
+
+echo "Installing XFCE, Nvidia drivers and switch config..."
+zypper -y in /L4T-Packages-Repository/*.rpm
+zypper -y up && zypper -t pattern xfce && zypper -y clean all 
 systemctl enable r2p bluetooth NetworkManager
-rm /etc/X11/xorg.conf.d/20-fbdev.conf
-# Change DM to lightDM here
-echo "keyboard=onboard" >> /etc/lightdm/lightdm-gtk-greeter.conf
+echo "Done!"
 
+echo "Fixing boot stuff..."
+rm -r /etc/X11/xorg.conf.d/20-fbdev.conf 
+update-alternatives --set default-displaymanager /usr/lib/X11/displaymanagers/lightdm
+echo "keyboard=onboard" >> /etc/lightdm/lightdm-gtk-greeter.conf
+echo "Done!"
+
+echo "Configuring user..."
 groupadd wheel
 useradd -m -G wheel,video,audio,users -s /bin/bash suse
 echo "suse:suse" | chpasswd
-sed -i 's/# %wheel        ALL=(ALL) ALL/%wheel        ALL=(ALL) ALL/g' >> # visudo file here 
-
-ldconfig
+sed -i 's/# %wheel        ALL=(ALL) ALL/%wheel        ALL=(ALL) ALL/g' >> /etc/sudoers
+echo "Done!"
